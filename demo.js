f81f7d9b7cf3a8d7e20cf7da7270753b4acdfa7d
wirachat <wirachat2you@hotmail.com>
2015-05-12 18:41:03 +0700

    add to git

---
+++ demo.js
@@ -0,0 +1,12 @@
+var stockthai = require("./stockthai");
+
+var options = { stock_quotes: ['mint','cpn','cpall']};
+
+stockthai.get(options,function(quote,result){
+
+  console.log( "Current Price of  "+quote+" is ---->"+result.price);
+
+  //see other fields  
+  console.log(JSON.stringify(result));
+
+});
---
+++ stockthai.js
@@ -0,0 +1,83 @@
+
+var Client = require('node-rest-client').Client;
+var cheerio = require('cheerio')
+var client = new Client();
+var request = require('request');
+var iconv = require('iconv-lite');
+//base service
+
+const BASE_SERVICE = 'http://www.settrade.com/C04_01_stock_quote_p1.jsp?txtSymbol=';
+
+var stockthai = {
+  
+  lastResult: [],
+  get:function (options,callback){
+  
+    var self = this;
+
+    if(!Array.isArray(options.stock_quotes))
+    {
+    	options.stock_quotes = [options.stock_quotes];
+    }
+    options.stock_quotes.forEach(function(item){  
+
+	    var cookie = request.cookie('IPO_Language=English');
+		var request_options = {
+	    url: BASE_SERVICE+item,
+	    method: 'GET',
+	    headers: {
+	        'User-Agent': 'Mozilla/5.0',
+	            'Cookie': cookie,
+	            'Accept': '/',
+	            'Connection': 'keep-alive'
+	    }};
+
+
+		request(request_options, function (error, response, body) {
+
+			
+	  		if (!error && response.statusCode == 200) {
+
+	  		
+	  		  $ = cheerio.load(body);
+	  		  var d = $(".txt13").find('.fr');
+
+	          var stockObj = {
+		          name :$(".title-index").html(),
+		          price:$(".txt48").html(),
+		          company:$(".txt19").html(),
+				  prior:$(d[0]).html(),
+				  open:$(d[2]).html(),
+				  valume:$(d[1]).html(),
+				  value:$(d[3]).html(),
+				  pervalue:$(d[5]).html(),
+				  height:$(d[4]).html(),
+				  low:$(d[6]).html(),
+				  average_price:$(d[8]).html(),
+				  ceiling:$(d[7]).html(),
+				  floor:$(d[9]).html()
+			  }
+	           
+	           //debug stock object;
+	  			//console.log(stockObj);
+	  			callback(item,stockObj);
+
+	     	}
+	  	});
+	       
+	});//end option.stock_quotes loop
+  }
+
+
+}
+
+
+module.exports  = stockthai;
+/*var myOptions = { stock_quotes: 'mint' };
+
+stockthai.get(myOptions,function(result){ 
+
+	console.log(result);
+
+});
+*/
